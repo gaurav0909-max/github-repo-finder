0a9e417a3253f6d682bf3cce924e0ab8b59afc1b
Gaurav Patel <gauravp.itpath@gmail.com>
2025-01-08 11:42:16 +0530

    Added suspense in github page

--- src/app/github/page.tsx
+++ src/app/github/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 import LoaderPage from "./../../components/Loader/index";
 import NotFoundPage from "./../../components/not-found/index";
@@ -12,8 +12,8 @@ import { token } from "./../../lib/helper";
 export default function GitHubPage() {
   const searchParams = useSearchParams();
   const searchType = searchParams.get("searchType") || ""; // Get searchType from URL
-
   const usernameFromUrl = searchParams.get("username") || "";
+
   const { profile, repos, users, loading, error } = useGitHubData(
     usernameFromUrl,
     token,
@@ -29,23 +29,33 @@ export default function GitHubPage() {
   }
 
   return (
-    <div className="min-h-screen bg-gray-900">
-      <section className="max-w-7xl mx-auto px-4 py-8">
-        <div className="text-left mb-12">
-          <GithubHeader
-            profile={profile}
-            searchType={searchType}
-            username={usernameFromUrl}
-            repos={repos}
-            users={users}
-          />
-        </div>
+    <Suspense fallback={<LoaderPage />}>
+      <div className="min-h-screen bg-gray-900">
+        <section className="max-w-7xl mx-auto px-4 py-8">
+          <div className="text-left mb-12">
+            <GithubHeader
+              profile={profile}
+              searchType={searchType}
+              username={usernameFromUrl}
+              repos={repos}
+              users={users}
+            />
+          </div>
 
-        <section className="mt-8">
-          {searchType === "repos" && <ReposFilter repos={repos} />}
-          {searchType === "users" && <UsersFilter users={users} />}
+          <section className="mt-8">
+            {searchType === "repos" && (
+              <Suspense fallback={<LoaderPage />}>
+                <ReposFilter repos={repos} />
+              </Suspense>
+            )}
+            {searchType === "users" && (
+              <Suspense fallback={<LoaderPage />}>
+                <UsersFilter users={users} />
+              </Suspense>
+            )}
+          </section>
         </section>
-      </section>
-    </div>
+      </div>
+    </Suspense>
   );
 }
